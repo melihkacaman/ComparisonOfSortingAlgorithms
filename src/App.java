0c833e9f7e76ac15b9f0d9163cbaa1a7989a313f
melih <melih.kacaman@stu.fsm.edu.tr>
2022-12-03 22:48:51 +0300

    insertion sort is done.

--- src/App.java
+++ src/App.java
@@ -2,14 +2,19 @@ import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        InsertionSort insertionSort = new InsertionSort();  
+        Integer[] data_10 = FakeData.generaIntegers(10); 
         
-        Integer[] ins = {1,2,19,19,3,5};
-        
-        Worker.executeAndMeasure(insertionSort, ins);
+        Integer[] data_1000 = FakeData.generaIntegers(1000);
+        Integer[] data_5000 = FakeData.generaIntegers(5000);
+        Integer[] data_10000 = FakeData.generaIntegers(10000);
+        Integer[] data_50000 = FakeData.generaIntegers(50000);
+        Integer[] data_100000 = FakeData.generaIntegers(100000);
+        Integer[] data_500000 = FakeData.generaIntegers(500000);
+        Integer[] data_1000000 = FakeData.generaIntegers(1000000);
+        Integer[] data_5000000 = FakeData.generaIntegers(5000000);
 
-        for (Integer integer : ins) {
-            System.out.print(integer + " ");
-        }
+
+        InsertionSort insertionSort = new InsertionSort();
+        Worker.executeAndMeasure(insertionSort, data_500000.clone());
     }
 }
--- src/FakeData.java
+++ src/FakeData.java
@@ -2,13 +2,20 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class FakeData {
-    public static ArrayList<Integer> generaIntegers(int n) {
+    public static Integer[] generaIntegers(int n) {
         ArrayList<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < n; i++)
             numbers.add(i);
 
         Collections.shuffle(numbers);
 
-        return numbers;
+        return numbers.toArray(new Integer[0]);
+    }
+
+    public static void showIntegers(Integer[] list){
+        for (Integer integer : list) {
+            System.out.print(integer + " ");
+        }
+        System.out.println();
     }
 }
